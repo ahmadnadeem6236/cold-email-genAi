440e98ca58e210a201ddc733f0ea112ff71de7d5
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-10-22 14:01:59 +0530

    refactor

--- app/main.py
+++ app/main.py
@@ -2,11 +2,10 @@ import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
 from chains import Chain
-from portfolio import Portfolio
 from utils import clean_text
 
 
-def create_streamlit_app(llm, portfolio, clean_text):
+def create_streamlit_app(llm,  clean_text):
     st.title("📧 Cold Mail Generator")
     url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-33460")
     submit_button = st.button("Submit")
@@ -15,12 +14,10 @@ def create_streamlit_app(llm, portfolio, clean_text):
         try:
             loader = WebBaseLoader([url_input])
             data = clean_text(loader.load().pop().page_content)
-            portfolio.load_portfolio()
             jobs = llm.extract_jobs(data)
             for job in jobs:
                 skills = job.get('skills', [])
-                links = portfolio.query_links(skills)
-                email = llm.write_mail(job, links)
+                email = llm.write_mail(job)
                 st.code(email, language='markdown')
         except Exception as e:
             st.error(f"An Error Occurred: {e}")
@@ -28,8 +25,7 @@ def create_streamlit_app(llm, portfolio, clean_text):
 
 if __name__ == "__main__":
     chain = Chain()
-    portfolio = Portfolio()
     st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
-    create_streamlit_app(chain, portfolio, clean_text)
+    create_streamlit_app(chain,  clean_text)
 
 
